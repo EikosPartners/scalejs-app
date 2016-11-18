96638ba7220659eca27e5a6f7a0b93ecd66199e3
Gucciardo <erica.gucciardo@eikospartners.com>
2016-11-18 14:57:40 -0500

    Add Adapter and Input

--- public/src/app/modules.js
+++ public/src/app/modules.js
@@ -1,4 +1,5 @@
 // Load common imports:
 import 'scalejs.metadatafactory-common/dist/template/templateModule';
-
+import 'scalejs.metadatafactory-common/dist/adapter/adapterModule';
+import 'scalejs.metadatafactory-common/dist/input/inputModule';
 // Load app-specific modules:
